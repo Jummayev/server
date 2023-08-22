098ccae4434c37b56dcc53f72c6426a0a7fee4fa
Nurbek Jummayev <jummayevnurbek279@gmail.com>
2023-08-23 00:53:16 +0500

    main

--- server.sh
+++ server.sh
@@ -1,109 +1,110 @@
 #!/bin/bash
+
+#install tool
+echo "Install wget, unzip, nano, curl applications "
+sleep 5
+sudo dnf install wget unzip nano curl
+#================================================================================
+#install nginx
+echo "Started installing nginx... "
+sleep 5
+
+dnf install nginx -y
+
+systemctl start nginx
+
+firewall-cmd --permanent --add-service=http
+
+firewall-cmd --permanent --add-service=https
+
+firewall-cmd --reload
+
+#================================================================================
+#install php
+echo "Started installing php... "
+sleep 5
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+dnf install epel-release -y
+
+dnf module list php
+
+dnf module enable php:remi-8.1 -y
+
+dnf install php -y
+
+php -v
+
+sleep 5
+
+dnf install php-redis php-pgsql php-zip php-soap php-gd php-fpm php-cli php-json -y
+
+systemctl start php-fpm
+
+systemctl enable php-fpm
+
+PHP_FPM_CONFIG_FILE=/etc/php-fpm.d/www.conf
+sed -i "s/user = .*/user = nginx/" "$PHP_FPM_CONFIG_FILE"
+sed -i "s/group = .*/group = nginx/" "$PHP_FPM_CONFIG_FILE"
+sed -i "s/listen.owner = .*/listen.owner = nginx/" "$PHP_FPM_CONFIG_FILE"
+sed -i "s/listen.group = .*/listen.group = nginx/" "$PHP_FPM_CONFIG_FILE"
+
+systemctl reload php-fpm
+
+systemctl restart php-fpm
+
+#================================================================================
+echo "Started installing composer... "
+sleep 5
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+
+# shellcheck disable=SC2034
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+composer
+
+#================================================================================
+#postgres install
+echo "Started installing postgres... "
+sleep 5
+dnf module list postgresql
+
+dnf module enable postgresql:15 -y
+
+dnf install postgresql-server -y
+
+postgresql-setup --initdb
+
+systemctl start postgresql
+
+systemctl enable postgresql
+
+systemctl restart postgresql
+
+#step 2 config postgres
+
+#grep listen_addresses /var/lib/pgsql/data/postgresql.conf
 #
-##install tool
-#echo "Install wget, unzip, nano, curl applications "
-#sleep 5
-#sudo dnf install wget unzip nano curl
+#grep -v -E "^#|^$" /var/lib/pgsql/data/pg_hba.conf
+
+#firewall-cmd --add-service=postgresql
 #
-##install nginx
-#echo "Started installing nginx... "
-#sleep 5
-#
-#dnf install nginx -y
-#
-#systemctl start nginx
-#
-#firewall-cmd --permanent --add-service=http
-#
-#firewall-cmd --permanent --add-service=https
-#
-#firewall-cmd --reload
-#
-#
-##install php
-#echo "Started installing php... "
-#sleep 5
-#dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-#
-#dnf install epel-release -y
-#
-#dnf module list php
-#
-#dnf module enable php:remi-8.1 -y
-#
-#dnf install php -y
-#
-#php -v
-#
-#sleep 5
-#
-#dnf install php-redis php-pgsql php-zip php-soap php-gd php-fpm php-cli php-json -y
-#
-#systemctl start php-fpm
-#
-#systemctl enable php-fpm
-#
-#PHP_FPM_CONFIG_FILE=/etc/php-fpm.d/www.conf
-#sed -i "s/user = .*/user = nginx/" "$PHP_FPM_CONFIG_FILE"
-#sed -i "s/group = .*/group = nginx/" "$PHP_FPM_CONFIG_FILE"
-#sed -i "s/listen.owner = .*/listen.owner = nginx/" "$PHP_FPM_CONFIG_FILE"
-#sed -i "s/listen.group = .*/listen.group = nginx/" "$PHP_FPM_CONFIG_FILE"
-#
-#systemctl reload php-fpm
-#
-#systemctl restart php-fpm
-#
-#
-#echo "Started installing composer... "
-#sleep 5
-#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-#
-#HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
-#
-#php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-#
-#composer
-#
-#
-##postgres install
-#echo "Started installing postgres... "
-#sleep 5
-#dnf module list postgresql
-#
-#dnf module enable postgresql:15 -y
-#
-#dnf install postgresql-server -y
-#
-#postgresql-setup --initdb
-#
-#systemctl start postgresql
-#
-#systemctl enable postgresql
-#
-#systemctl restart postgresql
-#
-##step 2 config postgres
-#
-##grep listen_addresses /var/lib/pgsql/data/postgresql.conf
-##
-##grep -v -E "^#|^$" /var/lib/pgsql/data/pg_hba.conf
-#
-##firewall-cmd --add-service=postgresql
-##
-##firewall-cmd --runtime-to-permanent
-#
-#
-##install redis
-##https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-redis-on-centos-8
-#dnf install redis
-#
-#nano /etc/redis.conf
-#
-##change supervised not to supervised systemd
-#
-#systemctl start redis.service
-#
-#systemctl enable redis
+#firewall-cmd --runtime-to-permanent
+
+#================================================================================
+#install redis
+#https://www.digitalocean.com/community/tutorials/how-to-install-and-secure-redis-on-centos-8
+dnf install redis
+
+nano /etc/redis.conf
+
+#change supervised not to supervised systemd
+
+systemctl start redis.service
+
+systemctl enable redis
 
 #nano /etc/redis.conf
 REDIS_CONFIG_FILE=/etc/redis.conf
